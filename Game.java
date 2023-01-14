22107250f2be4e38e105878c58b2aecf16168d6b
JoshuaMather4500 <joshuaiain@gmail.com>
2023-01-14 17:44:01 +0000

    F

--- Game.java
+++ Game.java
@@ -4,19 +4,34 @@ import helpers.InputReader;
 
 public class Game
 {
-    System.out.println("What do you want to do? ")
-            break
-    System.out.println("1 Start Game" break "2 Exit")
+    private void mainMenu;
 
-            int menuOption=
-
-    switch
     {
-        option1: ;
-        break;
-        option2: ;
-    }
-    int menuCommand;
 
+        for (int menuOption == 1; menuOption = 2;)
+
+        System.out.println("What do you want to do? ");
+        System.out.println("1 Start Game " + System.lineSeparator() + "2 Exit");
+
+
+        int menuOption = InputReader.getInt("Please enter a menu option");
+
+        switch(menuOption.get.menuOption)
+        {
+            case0 :
+            menuOption = 1;
+
+            case1:
+            menuOption = 2;
+
+            default:
+            System.out.println("Please enter a valid option");
+            break;
+
+        }
+        int menuCommand;
+
+        return menuOption;
+    }
 
 }
