f412be5523278056dd03edba13faac0bd3fda429
wmather123 <22200231@bucks.ac.uk>
2023-01-17 14:53:54 +0000

    Made the dice a single function

--- src/DiceFighter/Goblin.java
+++ src/DiceFighter/Goblin.java
@@ -1,4 +1,5 @@
-package src.DiceFighter;
+package src;
+import helpers.InputReader;
 
 public class Goblin
 {
@@ -9,11 +10,8 @@ public class Goblin
     private int armourClass = 15; //This value is how hard it is to hit the goblin
    private int health = 10; // this value is the total hp of the goblin
    private int attackBonus = 2; // this value is what the goblin will add to its d20 roll in order to hit the player
-    public int damage; // this value is how much the goblin will reduce the players hp by on a hit
+    private int damage; // this value is how much the goblin will reduce the players hp by on a hit
         {
-            Game game;
-                    game = new Game();
-            game.diceFour();
         }
 
    public Goblin()
